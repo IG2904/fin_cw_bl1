12365c638eb3c5876ddde409ddc73417605ad08e
IG2904 <130046351+IG2904@users.noreply.github.com>
2024-04-18 10:14:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] inputArray = { "apple", "2", "banana", "cat", "dog", "elephant", "Russia", "Denmark", "Kazan" };
+        string[] newArray = FilterArray(inputArray);
+
+        foreach (string item in newArray)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    static string[] FilterArray(string[] inputArray)
+    {
+        int count = 0;
+        foreach (string item in inputArray)
+        {
+            if (item.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] newArray = new string[count];
+        int index = 0;
+        foreach (string item in inputArray)
+        {
+            if (item.Length <= 3)
+            {
+                newArray[index] = item;
+                index++;
+            }
+        }
+
+        return newArray;
+    }
+}
